a97cf1102828b06ec4db2ffc956dc268ec687f2e
Josue Exhume <exhumejosue@gmail.com>
2018-02-22 23:42:48 -0500

    Changed the ascending and descending method definition for bubble sort to be of type T so that it can take and sort any type that implements the IComparable interface

--- Algorithm/BubbleSort.cs
+++ Algorithm/BubbleSort.cs
@@ -9,41 +9,41 @@ namespace Algorithm
     {
 
         /// <summary>
-        /// Sorts an array of integer values using the bubble sort algorithm in ascending order
+        /// Sorts an array of arbitrary type <typeparamref name="T"/> in ascending order, that implements the interface IComparable.
         /// </summary>
-        /// <param name="intArray"></param>
-        /// <returns>An array of sorted integers in ascending order</returns>
-        public int[] SortAscending(int[] intArray)
+        /// <param name="array"></param>
+        /// <returns>An array of generic types in ascending order</returns>
+        public T[] SortAscending<T>(T[] array) where T : IComparable<T>
         {
-            for (int j = 0; j < intArray.Length; j++)
+            for (int j = 0; j < array.Length; j++)
             {
-                for (int i = intArray.Length - 1; i > j; i--)
+                for (int i = array.Length - 1; i > j; i--)
                 {
-                    if (intArray[i - 1] > intArray[i])
+                    if (array[i - 1].CompareTo(array[i]) > 0)
                     {
-                        int temp = intArray[i - 1];
-                        intArray[i - 1] = intArray[i];
-                        intArray[i] = temp;
+                        T temp = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = temp;
                     }
                 }
             }
-            return intArray;
+            return array;
         }
 
         /// <summary>
-        /// Sorts an array of integer values using the bubble sort algorithm in descending order
+        /// Sorts an array of arbitrary type <typeparamref name="T"/> in descending order, that implements the interface IComparable.
         /// </summary>
         /// <param name="intArray"></param>
-        /// <returns>An array of sorted integers in descending order</returns>
-        public int[] SortDescending(int[] intArray)
+        /// <returns>An array of generic types in descending order</returns>
+        public T[] SortDescending<T>(T[] intArray) where T : IComparable<T>
         {
             for (int j = 0; j < intArray.Length; j++)
             {
                 for (int i = intArray.Length - 1; i > j; i--)
                 {
-                    if (intArray[i - 1] < intArray[i])
+                    if (intArray[i - 1].CompareTo(intArray[i]) < 0)
                     {
-                        int temp = intArray[i - 1];
+                        T temp = intArray[i - 1];
                         intArray[i - 1] = intArray[i];
                         intArray[i] = temp;
                     }
