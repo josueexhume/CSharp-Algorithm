7448048731ab59e25be8839dde5abf3eecc309b1
Josue Exhume <exhumejosue@gmail.com>
2018-02-25 17:27:13 -0500

    bubble sort methods are now static

--- Algorithm/BubbleSort.cs
+++ Algorithm/BubbleSort.cs
@@ -13,7 +13,7 @@ namespace Algorithm
         /// </summary>
         /// <param name="array"></param>
         /// <returns>An array of generic types in ascending order</returns>
-        public T[] SortAscending<T>(T[] array) where T : IComparable<T>
+        public static T[] SortAscending<T>(T[] array) where T : IComparable<T>
         {
             for (int j = 0; j < array.Length; j++)
             {
@@ -33,23 +33,23 @@ namespace Algorithm
         /// <summary>
         /// Sorts an array of arbitrary type <typeparamref name="T"/> in descending order, that implements the interface IComparable.
         /// </summary>
-        /// <param name="intArray"></param>
+        /// <param name="array"></param>
         /// <returns>An array of generic types in descending order</returns>
-        public T[] SortDescending<T>(T[] intArray) where T : IComparable<T>
+        public static T[] SortDescending<T>(T[] array) where T : IComparable<T>
         {
-            for (int j = 0; j < intArray.Length; j++)
+            for (int j = 0; j < array.Length; j++)
             {
-                for (int i = intArray.Length - 1; i > j; i--)
+                for (int i = array.Length - 1; i > j; i--)
                 {
-                    if (intArray[i - 1].CompareTo(intArray[i]) < 0)
+                    if (array[i - 1].CompareTo(array[i]) < 0)
                     {
-                        T temp = intArray[i - 1];
-                        intArray[i - 1] = intArray[i];
-                        intArray[i] = temp;
+                        T temp = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = temp;
                     }
                 }
             }
-            return intArray;
+            return array;
         }
 
     }
