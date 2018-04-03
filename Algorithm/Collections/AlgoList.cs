9a545f97b6500345ef300af4314418b5b2ccbdc8
Josue Exhume <exhumejosue@gmail.com>
2018-04-02 23:32:50 -0400

    Documented AlgoList

--- Algorithm/Collections/AlgoList.cs
+++ Algorithm/Collections/AlgoList.cs
@@ -9,23 +9,48 @@ using System.Threading.Tasks;
 
 namespace Algorithm.Collections
 {
-    public class AlgoList<T> : IList<T>
+    public partial class AlgoList<T> : IList<T>
     {
 
+        /// <summary>
+        /// The default size
+        /// </summary>
         const int DefaultSize = 5;
+        /// <summary>
+        /// The capacity
+        /// </summary>
         public static int _capacity;
+        /// <summary>
+        /// Gets the capacity.
+        /// </summary>
+        /// <value>
+        /// The capacity.
+        /// </value>
         public int Capacity => _capacity;
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public int Count { get; private set; } = 0;
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.
+        /// </summary>
         public bool IsReadOnly { get; }
         private T[] _items;
 
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlgoList{T}"/> class.
+        /// </summary>
         public AlgoList()
         {
             _capacity = DefaultSize;
             _items = new T[_capacity];
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlgoList{T}"/> class.
+        /// </summary>
+        /// <param name="capacity">The capacity.</param>
         public AlgoList(int capacity)
         {
             _capacity = capacity;
@@ -33,22 +58,42 @@ namespace Algorithm.Collections
         }
 
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             return ((ICollection<T>)_items).GetEnumerator();
         }
 
 
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Gets the enumerator.
+        /// </summary>
+        /// <returns></returns>
         public AlgoListEnumerator GetEnumerator()
         {
             return new AlgoListEnumerator(_items);
         }
 
+        /// <summary>
+        /// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
         public void Add(T item)
         {
             if (Count == _capacity)
@@ -59,6 +104,9 @@ namespace Algorithm.Collections
             Count++;
         }
 
+        /// <summary>
+        /// Increases the capacity.
+        /// </summary>
         public void IncreaseCapacity()
         {
             _capacity = (_capacity * 2);
@@ -67,21 +115,42 @@ namespace Algorithm.Collections
 
         }
 
+        /// <summary>
+        /// Inserts an item to the <see cref="T:System.Collections.Generic.IList`1" /> at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which <paramref name="item" /> should be inserted.</param>
+        /// <param name="item">The object to insert into the <see cref="T:System.Collections.Generic.IList`1" />.</param>
         public void Insert(int index, T item)
         {
             _items[index] = item;
         }
 
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public void Clear()
         {
             Count = 0;
         }
 
+        /// <summary>
+        /// Determines whether the <see cref="T:System.Collections.Generic.ICollection`1" /> contains a specific value.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        ///   <see langword="true" /> if <paramref name="item" /> is found in the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool Contains(T item)
         {
             return IndexOf(item) > -1;
         }
 
+        /// <summary>
+        /// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1" /> to an <see cref="T:System.Array" />, starting at a particular <see cref="T:System.Array" /> index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="T:System.Array" /> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1" />. The <see cref="T:System.Array" /> must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in <paramref name="array" /> at which copying begins.</param>
+        /// <exception cref="Exception">The array that's being copyied into doesn't have enough space</exception>
         public void CopyTo(T[] array, int arrayIndex)
         {
             ICollection<T> arrayCollection = array;
@@ -103,16 +172,36 @@ namespace Algorithm.Collections
 
         }
 
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        ///   <see langword="true" /> if <paramref name="item" /> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, <see langword="false" />. This method also returns <see langword="false" /> if <paramref name="item" /> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
         public bool Remove(T item)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Removes the <see cref="T:System.Collections.Generic.IList`1" /> item at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to remove.</param>
+        /// <exception cref="NotImplementedException"></exception>
         public void RemoveAt(int index)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Determines the index of a specific item in the <see cref="T:System.Collections.Generic.IList`1" />.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.IList`1" />.</param>
+        /// <returns>
+        /// The index of <paramref name="item" /> if found in the list; otherwise, -1.
+        /// </returns>
         public int IndexOf(T item)
         {
 
@@ -128,43 +217,19 @@ namespace Algorithm.Collections
         }
 
 
+        /// <summary>
+        /// Gets or sets the <see cref="T"/> at the specified index.
+        /// </summary>
+        /// <value>
+        /// The <see cref="T"/>.
+        /// </value>
+        /// <param name="index">The index.</param>
+        /// <returns></returns>
         public T this[int index]
         {
             get => _items[index];
             set => _items[index] = value;
         }
-
-        #region IEnumerator Implementation
-
-        public class AlgoListEnumerator : IEnumerator
-        {
-            private readonly T[] _items;
-            private int _capacity { get; } = 0;
-
-            public AlgoListEnumerator(T[] items)
-            {
-                _items = items;
-                _capacity = items.Length;
-
-            }
-
-            public bool MoveNext()
-            {
-                _position++;
-                return (_position < _capacity);
-            }
-
-            private int _position = -1;
-
-            public void Reset()
-            {
-                _position = -1;
-            }
-
-            public object Current => _items[_position];
-        }
-
-        #endregion
 
     }
 }
